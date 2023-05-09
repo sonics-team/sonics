46a6ec40176ee97ee7d6a8ba39cfa8ef254dce5b
Aous Mohammad <a.mohammad@work-with-nerds.ca>
2023-05-09 13:46:20 +0300

    fix button missing onClick

--- packages/sonics-core/src/Button.tsx
+++ packages/sonics-core/src/Button.tsx
@@ -1,11 +1,12 @@
 import * as React from "react";
 
 export interface ButtonProps {
+  onClick: React.MouseEventHandler<HTMLButtonElement>;
   children: React.ReactNode;
 }
 
 export function Button(props: ButtonProps) {
-  return <button>{props.children}</button>;
+  return <button onClick={props.onClick}>{props.children}</button>;
 }
 
 Button.displayName = "Button";
